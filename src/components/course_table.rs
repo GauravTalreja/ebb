4a599c208f123b812a7b23b31970e994fdfa885d
SallyHong2347 <hongsally192@gmail.com>
2023-06-16 20:42:17 -0400

    modify examples

--- src/components/course_table.rs
+++ src/components/course_table.rs
@@ -8,7 +8,7 @@ pub fn CourseTable<G: Html>(
     view! { cx,
         div (class="overflow-x-auto w-full shadow-md rounded-md") {
             // TODO: change header color, change hover color
-            table (class="table table-auto table-md w-full") {
+            table (class="table w-full") {
                 // table header
                 thead(class="") {
                     tr() {
@@ -21,10 +21,27 @@ pub fn CourseTable<G: Html>(
                 }
                 // table content
                 tbody() {
-                    TableContent(name = "testing".to_string())
-                    TableContent(name = "testing".to_string())
-                    TableContent(name = "testing".to_string())
-                    TableContent(name = "testing".to_string())
+                    TableContent(idx="1".to_string(),
+                                code="CS136".to_string(), 
+                                coursename="Elementary Algorithm Design and Data Abstraction".to_string(),
+                                location="ONLN/UW".to_string(),
+                                status="Open".to_string())
+                    TableContent(idx="2".to_string(),
+                                code="CS246".to_string(), 
+                                coursename="Object-Oriented Software Development".to_string(),
+                                location="ONLN/UW".to_string(),
+                                status="Closed".to_string())
+                    TableContent(idx="3".to_string(),
+                                code="MUSIC140".to_string(), 
+                                coursename="Popular Music and Culture".to_string(),
+                                location="ONLN".to_string(),
+                                status="Open".to_string())
+                    TableContent(idx="4".to_string(),
+                                code="ECON102".to_string(), 
+                                coursename="Introduction to Macroeconomics".to_string(),
+                                location="REN".to_string(),
+                                status="Open".to_string())
+                    
                 }
 
             }
@@ -39,7 +56,11 @@ pub fn CourseTable<G: Html>(
 pub struct TableContentProps {
     
     // TODO: include more content
-    name: String,
+    idx: String,
+    code: String,
+    coursename: String,
+    location: String,
+    status: String,
     // subject + code e.g. CS136
     // coursecode: String,
     // courseName: String, 
@@ -52,15 +73,15 @@ pub struct TableContentProps {
 // (ex. Math Course -> Primary Color, Eng Course -> Secondary Color etc.)
 #[component]
 fn TableContent<G: Html>(cx: Scope, 
-    TableContentProps { name }: TableContentProps) -> View<G> {
+    TableContentProps { idx, code, coursename, location, status }: TableContentProps) -> View<G> {
     view! { cx,
         // TODO: change hover color hover:bg-primary-content
         tr (class="hover ") {
-            th() { "1"} // row index
-            td() { (name) }
-            td() { (name) }
-            td() { (name) }
-            td() { (name) }       
+            th() { (idx) } // row index
+            td() { (code) }
+            td() { (coursename) }
+            td() { (location) }
+            td() { (status) }       
             
         }
     }
