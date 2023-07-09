733f056a0576f8a2bef740e0bca48c3b8139756b
SaumyaK <saumyakhati1@gmail.com>
2023-07-09 12:06:12 -0400

    Redesign Schema + Fix issues + Update Queries
    - Fixed issues mentioned in #32.
    - Made changes based on TA feedback in Milestone 1.
    - Additional schema improvements based on group discussion.
    - Updated insert queries for populating sample database.

--- sample/migrations/20230621162215_courses.sql
+++ sample/migrations/20230621162215_courses.sql
@@ -1,18 +1,20 @@
 -- Courses Table
 CREATE TABLE IF NOT EXISTS courses (
     id SERIAL PRIMARY KEY,
-    catalog_number SMALLINT NOT NULL,
+    catalog_number VARCHAR(10) NOT NULL,
     subject_code VARCHAR(10) NOT NULL,
     external_id VARCHAR(10) NOT NULL,
     academic_level VARCHAR(13) NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
+    requirements TEXT NULL,
+    enroll_consent TEXT NULL,
+    drop_consent TEXT NULL,
     prerequisites_id INTEGER NULL,
-    CONSTRAINT uk_courses_external_id UNIQUE (external_id),
     CONSTRAINT uk_courses_catalog_subject UNIQUE (catalog_number, subject_code),
     CONSTRAINT chk_courses_external_id_not_empty CHECK (external_id <> ''),
     CONSTRAINT chk_courses_subject_code_not_empty CHECK (subject_code <> ''),
-    CONSTRAINT chk_courses_academic_level CHECK (academic_level IN ('undergraduate', 'graduate')),
+    CONSTRAINT chk_courses_academic_level CHECK (academic_level IN ('UG', 'GRD')),
     CONSTRAINT chk_courses_title_not_empty CHECK (title <> '')
 );
 
@@ -35,14 +37,11 @@ CREATE TABLE IF NOT EXISTS course_offerings (
     course_id INTEGER NOT NULL,
     year SMALLINT NOT NULL,
     term VARCHAR(6) NOT NULL,
-    max_enrollment SMALLINT NOT NULL DEFAULT 0,
-    current_enrollment SMALLINT NOT NULL DEFAULT 0,
     CONSTRAINT uk_course_offerings_unique_data UNIQUE (course_id, year, term),
     CONSTRAINT fk_course_offerings_to_courses FOREIGN KEY (course_id)
         REFERENCES courses (id)
         ON DELETE CASCADE,
-    CONSTRAINT chk_course_offerings_term CHECK (term IN ('winter', 'fall', 'spring')),
-    CONSTRAINT chk_course_current_enrollment CHECK (current_enrollment <= max_enrollment)
+    CONSTRAINT chk_course_offerings_term CHECK (term IN ('Winter', 'Fall', 'Spring'))
 );
 
 -- Course Offerings Table Indices
@@ -52,23 +51,29 @@ CREATE INDEX IF NOT EXISTS idx_course_offerings_course_id
 CREATE INDEX IF NOT EXISTS idx_course_offerings_year_term
     ON course_offerings (year, term);
 
--- Course Schedule Table
-CREATE TABLE IF NOT EXISTS course_schedule (
+-- Class Schedule Table
+CREATE TABLE IF NOT EXISTS class_schedule (
     id SERIAL PRIMARY KEY,
-    section VARCHAR(30) NOT NULL,
+    class_section SMALLINT NOT NULL,
+    class_number SMALLINT NOT NULL,
+    component VARCHAR(10) NULL,
     start_time TIME,
     end_time TIME,
-    meeting_days VARCHAR(7) NOT NULL,
-    instructor_name VARCHAR(50) NOT NULL,
-    room_name VARCHAR(30) NOT NULL,
-    campus_name VARCHAR(50) NOT NULL,
+    monday BOOLEAN NOT NULL,
+    tuesday BOOLEAN NOT NULL,
+    wednesday BOOLEAN NOT NULL,
+    thursday BOOLEAN NOT NULL,
+    friday BOOLEAN NOT NULL,
+    saturday BOOLEAN NOT NULL,
+    sunday BOOLEAN NOT NULL,
+    instructor_name TEXT NULL,
+    location VARCHAR(50) NULL,
     course_offering_id INTEGER NOT NULL,
     max_enrollment SMALLINT NOT NULL DEFAULT 0,
     current_enrollment SMALLINT NOT NULL DEFAULT 0,
-    CONSTRAINT fk_course_schedule_to_courses FOREIGN KEY (course_offering_id)
+    CONSTRAINT fk_class_schedule_to_courses FOREIGN KEY (course_offering_id)
         REFERENCES course_offerings (id)
         ON DELETE CASCADE,
-    CONSTRAINT chk_meeting_days CHECK (meeting_days ~ '^[YN]{7}$'),
-    CONSTRAINT chk_course_schedule_current_enrollment CHECK (current_enrollment <= max_enrollment),
-    CONSTRAINT chk_course_schedule_times CHECK (end_time > start_time)
+    CONSTRAINT chk_class_schedule_times CHECK (end_time >= start_time),
+    CONSTRAINT uk_class_num_course_offering UNIQUE (class_number, course_offering_id)
 );
