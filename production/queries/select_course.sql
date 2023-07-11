1d3799c6618eed70a70aeac61820b88c280f59c6
Justin Kwan <justinpkwan@outlook.com>
2023-07-11 19:50:18 -0400

    Update production course list and detail queries to scan MV

--- production/queries/select_course.sql
+++ production/queries/select_course.sql
@@ -1,29 +1,25 @@
 SELECT
-    c.id AS id,
-    c.catalog_number AS catalog_number,
-    c.subject_code AS subject_code,
-    c.external_id AS external_id,
-    c.academic_level AS academic_level,
-    c.title AS title,
-    c.description AS description,
-    COALESCE(
-        NULLIF(array_agg(DISTINCT rc.subject_code || rc.catalog_number::TEXT), '{null}'),
-        '{}'
-    ) AS "required_prerequisites!: _",
-    COALESCE(
-        NULLIF(array_agg(DISTINCT oc.subject_code || oc.catalog_number::TEXT), '{null}'),
-        '{}'
-    ) AS "optional_prerequisites!: _"
+    id AS "id!",
+    catalog_number AS "catalog_number!",
+    subject_code AS "subject_code!",
+    external_id AS "external_id!",
+    academic_level AS "academic_level!",
+    title AS "title!",
+    description AS "description!",
+    required_prerequisites AS "required_prerequisites!: _",
+    optional_prerequisites AS "optional_prerequisites!: _"
 FROM
-    courses c
-    LEFT JOIN course_offerings co ON c.id = co.course_id
-    LEFT JOIN prerequisites p ON c.prerequisites_id = p.id
-    LEFT JOIN required_prerequisites rp ON p.id = rp.prerequisite_id
-    LEFT JOIN courses rc ON rp.course_id = rc.id
-    LEFT JOIN optional_prerequisites op ON p.id = op.prerequisite_id
-    LEFT JOIN courses oc ON op.course_id = oc.id
+    mv_courses c
 WHERE
     UPPER(c.subject_code) || c.catalog_number::VARCHAR = UPPER($1)
 GROUP BY
-    c.id
+    c.id,
+    c.catalog_number,
+    c.subject_code,
+    c.external_id,
+    c.academic_level,
+    c.title,
+    c.description,
+    c.required_prerequisites,
+    c.optional_prerequisites
 LIMIT 1;
