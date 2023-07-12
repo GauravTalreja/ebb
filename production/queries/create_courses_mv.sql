6ef41ae3445af889fb4daeab4f1257fc51c853a1
Justin-Kwan <37853180+Justin-Kwan@users.noreply.github.com>
2023-07-11 20:22:46 -0400

    Implement Tuned SQL Production Queries and Indexes for Courses (#45)
    * Update production course list and detail queries to scan MV
    
    * Create master courses MV and indices in migration
    
    * sqlx-data
    
    ---------
    
    Co-authored-by: GT <59036640+GauravTalreja@users.noreply.github.com>

--- production/queries/create_courses_mv.sql
+++ production/queries/create_courses_mv.sql
@@ -5,21 +5,36 @@ SELECT
     c.subject_code AS subject_code,
     c.title AS title,
     c.external_id AS external_id,
+    c.academic_level AS academic_level,
+    c.description AS description,
     (
         SELECT
-            COALESCE(NULLIF(json_agg(DISTINCT co.*)::TEXT, '[null]'), '[]')
+            COALESCE(NULLIF(jsonb_agg(DISTINCT co.*)::TEXT, '[null]'), '[]')
                 AS
                 offerings
         FROM
             course_offerings co
         WHERE
-                c.id = co.course_id AND
-                co.year = (SELECT EXTRACT(YEAR FROM CURRENT_DATE) AS current_year)
+            c.id = co.course_id AND
+            co.year = (SELECT EXTRACT(YEAR FROM CURRENT_DATE) AS current_year)
         LIMIT 1
-    ) AS offerings
+    ) AS offerings,
+    COALESCE(
+        NULLIF(array_agg(DISTINCT rc.subject_code || rc.catalog_number::TEXT), '{null}'),
+        '{}'
+    ) AS required_prerequisites,
+    COALESCE(
+        NULLIF(array_agg(DISTINCT oc.subject_code || oc.catalog_number::TEXT), '{null}'),
+        '{}'
+    ) AS optional_prerequisites
 FROM
     courses c
     LEFT JOIN course_offerings co ON c.id = co.course_id
+    LEFT JOIN prerequisites p ON c.prerequisites_id = p.id
+    LEFT JOIN required_prerequisites rp ON p.id = rp.prerequisite_id
+    LEFT JOIN courses rc ON rp.course_id = rc.id
+    LEFT JOIN optional_prerequisites op ON p.id = op.prerequisite_id
+    LEFT JOIN courses oc ON op.course_id = oc.id
 GROUP BY
     c.id,
     c.subject_code
