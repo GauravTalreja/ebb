7e7c0b737f7827e41e287a9d82e435a780cdca80
GT <59036640+GauravTalreja@users.noreply.github.com>
2023-07-26 02:20:44 -0400

    details

--- src/components/search_bar.rs
+++ src/components/search_bar.rs
@@ -1,14 +1,12 @@
+use models::CourseSummary;
 use perseus::prelude::*;
 use sycamore::prelude::*;
-
-#[cfg(client)]
-use models::CourseSummary;
 
 #[derive(Prop)]
 pub struct SearchBarProps<'a> {
     pub input: &'a RcSignal<String>,
     // TODO: This is a vector of strings for Milestone 0, but we should have a fancier state with (cached?) strongly typed search results
-    pub results: &'a RcSignal<Vec<String>>,
+    pub results: &'a RcSignal<Vec<CourseSummary>>,
 }
 
 // TODO: Improve this with support for mobile and arrow keys, see https://github.com/metonym/svelte-typeahead/blob/master/src/Typeahead.svelte for inspiration
@@ -21,7 +19,7 @@ pub fn SearchBar<'a, G: Html>(
     create_effect_scoped(cx, |cx| {
         if !input.get().is_empty() {
             spawn_local_scoped(cx, async {
-                let mut body: Vec<String> = reqwasm::http::Request::get(
+                let mut body = reqwasm::http::Request::get(
                     format!("/api/v1/courses/{}", input.get()).as_str(),
                 )
                 .send()
@@ -29,14 +27,13 @@ pub fn SearchBar<'a, G: Html>(
                 .unwrap()
                 .json::<Vec<CourseSummary>>()
                 .await
-                .unwrap()
-                .iter()
-                .map(|course| course.title.clone())
-                .collect();
+                .unwrap();
 
                 let max_len = 6;
                 if body.len() > max_len {
-                    body.resize(max_len, "".to_string())
+                    (0..(body.len() - max_len)).for_each(|_| {
+                        body.pop();
+                    })
                 }
                 results.set(body);
             })
@@ -63,11 +60,11 @@ pub fn SearchBar<'a, G: Html>(
                     Indexed(
                         iterable=results,
                         view=|cx, result| view! { cx,
-                            SearchResult(name=result.clone(), link=format!("c/{}",result))
+                            SearchResult(subject = result.subject_code, catalogue = result.catalog_number , title=result.title)
                         },
                     )
                     li {
-                        a (href=format!("courses/{}", input.get().as_str())) {
+                        a (href=format!("courses/{}", input.get().as_str()), class="bg-primary text-primary-content hover:bg-primary-focus") {
                             (format!("View all {} courses", input.get().as_str()))
                         }
                     }
@@ -79,8 +76,9 @@ pub fn SearchBar<'a, G: Html>(
 
 #[derive(Prop)]
 pub struct SearchResultProps {
-    name: String,
-    link: String,
+    subject: String,
+    catalogue: String,
+    title: String,
 }
 // TODO: Make results fancier than plain text
 // TODO: Use custom styles based on what the search result is, would require pattern matching a more strongly typed result
@@ -88,9 +86,14 @@ pub struct SearchResultProps {
 #[component]
 fn SearchResult<G: Html>(
     cx: Scope,
-    SearchResultProps { name, link }: SearchResultProps,
+    SearchResultProps {
+        subject,
+        catalogue,
+        title,
+    }: SearchResultProps,
 ) -> View<G> {
+    let path = subject.clone() + &catalogue;
     view! { cx,
-        li {a (href=link) { (name) }}
+        li {a (href=format!("c/{path}")) { (format!("{subject} {catalogue} - {title}")) }}
     }
 }
---
+++ src/templates/details.rs
@@ -0,0 +1,136 @@
+use crate::{
+    components::layout::{Layout, SearchBarProps, ThemeProps},
+    global_state::AppStateRx,
+};
+use models::{CourseDetail, CourseSummary};
+use perseus::prelude::*;
+use serde::{Deserialize, Serialize};
+use sycamore::prelude::*;
+
+#[derive(Serialize, Deserialize, ReactiveState, Clone)]
+#[rx(alias = "DetailStateRx")]
+pub struct DetailState {
+    path: String,
+    course_detail: CourseDetail,
+    search_input: String,
+    search_results: Vec<CourseSummary>,
+}
+
+fn details_page<'a, G: Html>(cx: BoundedScope<'_, 'a>, state: &'a DetailStateRx) -> View<G> {
+    let global_state = Reactor::<G>::from_cx(cx).get_global_state::<AppStateRx>(cx);
+    let theme_props = ThemeProps {
+        state: &global_state.theme,
+    };
+    let search_bar_props = SearchBarProps {
+        input: &state.search_input,
+        results: &state.search_results,
+    };
+
+    #[cfg(client)]
+    if !&state.path.get_untracked().is_empty() {
+        spawn_local_scoped(cx, async {
+            let body = reqwasm::http::Request::get(
+                format!("/api/v1/course/{}", &state.path.get_untracked()).as_str(),
+            )
+            .send()
+            .await
+            .unwrap()
+            .json::<CourseDetail>()
+            .await
+            .unwrap();
+
+            state.course_detail.set(body);
+        })
+    }
+
+    if !state.path.get().is_empty() {
+        view! { cx,
+            link ( rel="stylesheet", href="/tailwind.css")
+            Layout (search_bar=search_bar_props, theme=theme_props) {
+                div (class="prose m-16") {
+                    h1 { (state.course_detail.get().subject_code.clone() + " " + &state.course_detail.get().catalog_number + " - " + &state.course_detail.get().title) }
+                    p { (state.course_detail.get().description) }
+                    (if !state.course_detail.get().required_prerequisites.is_empty() {
+                        view! { cx,
+                            div {
+                                h2 { "Required Prerequisites:" }
+                                Keyed (
+                                    iterable=create_signal(cx, state.course_detail.get().required_prerequisites.clone()),
+                                    view=|cx, content| view! { cx,
+                                        p {(content)}
+                                    },
+                                    key=|content| content.clone(),
+                                )
+                            }
+                        }
+                    } else {
+                        view! {cx, }
+                    })
+                    (if !state.course_detail.get().optional_prerequisites.is_empty() {
+                        view! { cx,
+                            div {
+                                h2 { "Optional Prerequisites:" }
+                                Keyed (
+                                    iterable=create_signal(cx, state.course_detail.get().optional_prerequisites.clone()),
+                                    view=|cx, content| view! { cx,
+                                        p {(content)}
+                                    },
+                                    key=|content| content.clone(),
+                                )
+                            }
+                        }
+                    } else {
+                        view! {cx, }
+                    })
+                }
+            }
+        }
+    } else {
+        view! {cx, }
+    }
+}
+
+#[engine_only_fn]
+async fn get_build_state(info: StateGeneratorInfo<()>) -> DetailState {
+    DetailState {
+        path: info.path,
+        course_detail: CourseDetail {
+            id: -1,
+            catalog_number: "".to_owned(),
+            subject_code: "".to_owned(),
+            title: "Loading...".to_owned(),
+            external_id: "".to_owned(),
+            description: "".to_owned(),
+            academic_level: "".to_owned(),
+            optional_prerequisites: vec!["".to_owned()],
+            required_prerequisites: vec!["".to_owned()],
+        },
+        search_input: "".to_string(),
+        search_results: vec![],
+    }
+}
+
+#[engine_only_fn]
+async fn get_build_paths() -> BuildPaths {
+    BuildPaths {
+        paths: vec!["".to_owned()],
+        extra: ().into(),
+    }
+}
+
+#[engine_only_fn]
+fn head(cx: Scope, state: DetailState) -> View<SsrNode> {
+    view! { cx,
+        title { (state.course_detail.title) }
+    }
+}
+
+pub fn get_template<G: Html>() -> Template<G> {
+    Template::build("c")
+        .build_paths_fn(get_build_paths)
+        .build_state_fn(get_build_state)
+        .incremental_generation()
+        .view_with_state(details_page)
+        .head_with_state(head)
+        .build()
+}
