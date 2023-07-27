709f3fb4fd88e092edf39e7d5aa2f60b2c3b8099
Gaurav Talreja <59036640+GauravTalreja@users.noreply.github.com>
2023-07-26 23:31:17 -0400

    Show course offerings on the details page (#54)

--- src/templates/details.rs
+++ src/templates/details.rs
@@ -2,7 +2,7 @@ use crate::{
     components::layout::{Layout, SearchBarProps, ThemeProps},
     global_state::AppStateRx,
 };
-use models::{CourseDetail, CourseSummary};
+use models::{CourseDetail, CourseSummary, OfferingDetail};
 use perseus::prelude::*;
 use serde::{Deserialize, Serialize};
 use sycamore::prelude::*;
@@ -12,6 +12,7 @@ use sycamore::prelude::*;
 pub struct DetailState {
     path: String,
     course_detail: CourseDetail,
+    offering_details: Vec<OfferingDetail>,
     search_input: String,
     search_results: Vec<CourseSummary>,
 }
@@ -40,14 +41,46 @@ fn details_page<'a, G: Html>(cx: BoundedScope<'_, 'a>, state: &'a DetailStateRx)
             .unwrap();
 
             state.course_detail.set(body);
-        })
+        });
+
+        spawn_local_scoped(cx, async {
+            let body = reqwasm::http::Request::get(
+                format!("/api/v1/course_offerings/{}", &state.path.get_untracked()).as_str(),
+            )
+            .send()
+            .await
+            .unwrap();
+            use std::cmp::Ordering;
+            match body.json::<Vec<OfferingDetail>>().await {
+                Ok(mut details) => state.offering_details.set({
+                    details.iter_mut().for_each(|offering| {
+                        offering
+                            .schedules
+                            .0
+                            .sort_unstable_by(|a, b| a.class_section.cmp(&b.class_section));
+
+                        offering.schedules.0.sort_unstable_by(|a, b| {
+                            match (&a.component, &b.component) {
+                                (Some(a_str), Some(b_str)) => a_str.cmp(b_str),
+                                (Some(_), None) => Ordering::Less,
+                                (None, Some(_)) => Ordering::Greater,
+                                (None, None) => Ordering::Equal,
+                            }
+                        })
+                    });
+                    details
+                }),
+                Err(e) => web_log!("{body:?} {e}"),
+            }
+        });
     }
 
     if !state.path.get().is_empty() {
         view! { cx,
             link ( rel="stylesheet", href="/tailwind.css")
             Layout (search_bar=search_bar_props, theme=theme_props) {
-                div (class="prose m-16") {
+                div (class="prose prose-xl w-full mx-16") {
+                    div (class = "h-16")
                     h1 { (state.course_detail.get().subject_code.clone() + " " + &state.course_detail.get().catalog_number + " - " + &state.course_detail.get().title) }
                     p { (state.course_detail.get().description) }
                     (if !state.course_detail.get().required_prerequisites.is_empty() {
@@ -82,6 +115,85 @@ fn details_page<'a, G: Html>(cx: BoundedScope<'_, 'a>, state: &'a DetailStateRx)
                     } else {
                         view! {cx, }
                     })
+                    Keyed (
+                        iterable=&state.offering_details,
+                        view=|cx, offering| view! { cx,
+                            h2 { (format!("{} {}", offering.term, offering.year)) }
+                            Keyed (
+                                iterable=create_signal(cx, offering.schedules.0.clone()),
+                                view=|cx, schedule| view! { cx,
+                                    h3 {(format!("{} {}\n",
+                                        schedule.component.as_ref().unwrap_or(&"".to_owned()),
+                                        schedule.class_section,
+                                    ))
+                                    (match &schedule.instructor_name {
+                                        Some(instructor) => if !instructor.is_empty() {
+                                            format!(" - {instructor}")
+                                        } else {
+                                            "".to_owned()
+                                        },
+                                        None => "".to_owned()
+                                    })
+                                    (match &schedule.location {
+                                        Some(location) => if !location.is_empty() {
+                                            format!(" @ {}", location)
+                                        } else {
+                                            "".to_owned()
+                                        },
+                                        None => "".to_owned()
+                                    })
+                                    " on "
+                                    ({
+                                        let mut days = Vec::new();
+                                        if schedule.monday {
+                                            days.push("Monday");
+                                        }
+                                        if schedule.tuesday {
+                                            days.push("Tuesday");
+                                        }
+                                        if schedule.wednesday {
+                                            days.push("Wednesday");
+                                        }
+                                        if schedule.thursday {
+                                            days.push("Thursday");
+                                        }
+                                        if schedule.friday {
+                                            days.push("Friday");
+                                        }
+                                        if schedule.saturday {
+                                            days.push("Saturday");
+                                        }
+                                        if schedule.sunday {
+                                            days.push("Sunday");
+                                        }
+                                        // Join the days using the appropriate separators (e.g., "and" or ",")
+                                        match days.len() {
+                                            0 => "No days".to_owned(),
+                                            1 => days[0].to_owned(),
+                                            2 => format!("{} and {}", days[0], days[1]),
+                                            _ => {
+                                                let last_day = days.pop().unwrap();
+                                                let other_days = days.join(", ");
+                                                format!("{} and {}", other_days, last_day)
+                                            }
+                                        }
+                                    })
+                                    ", "
+                                    (schedule.start_time.format("%H:%M").to_string())
+                                    " - "
+                                    (schedule.end_time.format("%H:%M").to_string())
+                                    (format!(" ({}/{})",
+                                        schedule.current_enrollment,
+                                        schedule.max_enrollment,
+                                    ))
+                                    }
+                                },
+                                key=|content| content.clone(),
+                            )
+                        },
+                        key=|content| content.clone(),
+                    )
+                    div (class = "h-16")
                 }
             }
         }
@@ -102,9 +214,10 @@ async fn get_build_state(info: StateGeneratorInfo<()>) -> DetailState {
             external_id: "".to_owned(),
             description: "".to_owned(),
             academic_level: "".to_owned(),
-            optional_prerequisites: vec!["".to_owned()],
-            required_prerequisites: vec!["".to_owned()],
+            optional_prerequisites: vec!["null".to_owned()],
+            required_prerequisites: vec!["undefined".to_owned()],
         },
+        offering_details: vec![],
         search_input: "".to_string(),
         search_results: vec![],
     }
@@ -132,5 +245,6 @@ pub fn get_template<G: Html>() -> Template<G> {
         .incremental_generation()
         .view_with_state(details_page)
         .head_with_state(head)
+        .revalidate_after("1s")
         .build()
 }
